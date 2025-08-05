e996ec29cb902368aaa083148be41e1126c1c768
NoensLeftToe <sweta.negi208@gmail.com>
2025-08-06 01:05:08 +0530

    hello

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,11 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Rollo-35',
   description: 'Created with v0',
+  icons: {
+    icon: "/1.gif",
+  },
   generator: 'v0.dev',
 }
 
